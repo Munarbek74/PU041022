e22f805c670d67d88a28e96ebb678b4c7f907065
Munarbek74 <munarbekshorokhov@gmail.com>
2022-10-04 17:28:50 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    Solution solution = new Solution();
+        int n = 19;
+        if (solution.isHappy(n))
+            System.out.println("is Happy");
+        else
+            System.out.println("not is Happy");
+    }
+    }
